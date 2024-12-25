597301970b5f86a054235d8254cd28f3ffb8faa1
Adrian <adrian.suarez.alphen@gmail.com>
2024-12-25 17:43:55 -0500

    fix(start-course-section): fixed start course imagen importation

--- src/components/our-courses/OurCourses.tsx
+++ src/components/our-courses/OurCourses.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import OurCoursesInformation from "./components/OurCoursesInformation";
 import OurCoursesImage from "./components/OurCoursesImage";
+import OurCoursesFloatingItems from "./components/OurCoursesFloatingItems";
 
 export default function OurCourses() {
   return (
     <div className="relative overflow-visible bg-gradient-to-r from-gradient-from via-gradient-to to-gradient-to ">
+      {/* Floating items */}
+      <OurCoursesFloatingItems />
       <div className="container mx-auto px-4">
         <div className="flex flex-col-reverse lg:flex-row justify-between gap-8 md:gap-60">
           {/* Left side - Image */}
--- src/components/start-course/StartCourse.tsx
+++ src/components/start-course/StartCourse.tsx
@@ -1,17 +1,16 @@
 import StartCourseFloatingItems from "./components/StartCourseFloatingItems";
-import FloatingItems from "./components/StartCourseFloatingItems";
-
+import StartCourseImage from "./components/StartCourseImage";
 import StartCourseInformation from "./components/StartCourseInformation";
 
 export default function StartCourse() {
   return (
     <div className="relative overflow-visible bg-gradient-to-r from-gradient-from via-gradient-to to-gradient-to ">
       {/* Floating items */}
-      <FloatingItems />
+      <StartCourseFloatingItems />
       <div className="container mx-auto px-4">
         <div className="flex flex-col-reverse lg:flex-row justify-between gap-8 md:gap-60">
           {/* Left side - Image */}
-          <StartCourseFloatingItems />
+          <StartCourseImage />
           {/* Right side - Content */}
           <StartCourseInformation />
         </div>
