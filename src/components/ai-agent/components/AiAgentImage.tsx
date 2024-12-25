7fc3a806459dbaa899f40428da88f445140e8305
Adrian <adrian.suarez.alphen@gmail.com>
2024-12-25 15:47:30 -0500

    fix(ai-agent-section):fixed ai agent section image size

--- src/components/ai-agent/components/AiAgentImage.tsx
+++ src/components/ai-agent/components/AiAgentImage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function AiAgentImage() {
   return (
     <div className="flex w-full lg:w-auto justify-end">
-      <div className="w-[200px] xsplus:w-[340px] xs:w-[420px] sm:w-[600px] lg:w-[500px] xl:w-[700px] 2xl:w-[800px] h-[200px] xsplus:h-[300px] xs:h-[380px] sm:h-[500px] lg:h-[500px] xl:h-[600px] relative">
+      <div className="w-[200px] xsplus:w-[340px] xs:w-[420px] sm:w-[600px] lg:w-[600px] xl:w-[700px] 2xl:w-[800px] h-[200px] xsplus:h-[300px] xs:h-[380px] sm:h-[600px] lg:h-[600px] xl:h-[600px] relative">
         <Image
           src="/images/AiAgent.png"
           alt="Course instructor"
