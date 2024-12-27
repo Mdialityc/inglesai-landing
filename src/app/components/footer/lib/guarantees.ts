85f96f5108cda4d1f3724812150b33b5a4c1580a
Adrian <adrian.suarez.alphen@gmail.com>
2024-12-27 11:46:11 -0500

    feat(app): added footer

---
+++ src/app/components/footer/lib/guarantees.ts
@@ -0,0 +1,8 @@
+export const guarantees: string[] = [
+  "Cursos creados por IA y supervisados por profesores experimentados.",
+  "Correcciones ilimitadas de actividades tanto escritas como orales.",
+  "Te ayudamos a configurar tu GPT a medida.",
+  "Estadísticas, resultado y seguimiento de progreso.",
+  "Feedback completo en cada tarea.",
+  "Garantía de 15 días satisfecho o reembolsado.",
+];
---
+++ src/app/components/footer/lib/nagetations.ts
@@ -0,0 +1,23 @@
+interface Navegation {
+  name: string;
+  path: string;
+}
+
+export const navegations: Navegation[] = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Páginas",
+    path: "/pages",
+  },
+  {
+    name: "Servicios",
+    path: "/services",
+  },
+  {
+    name: "Sobre nosotros",
+    path: "/about-us",
+  },
+];
