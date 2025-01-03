c5f99ebe97e3a1a0694bc33bca43d83691a704a3
Adrian <adrian.suarez.alphen@gmail.com>
2025-01-03 15:59:59 -0500

    feat(inglesai-button): added inglesai-button animation

--- src/app/components/not-found/not-found-content.tsx
+++ src/app/components/not-found/not-found-content.tsx
@@ -6,11 +6,11 @@ import InglesaiButton from "@/components/ui/inglesai-button";
 import { Ghost } from "lucide-react";
 export default function NotFoundContent() {
   return (
-    <div className="max-w-md w-full space-y-8 text-center">
+    <div className="flex flex-col gap-7 items-center justify-center">
       <motion.div
         animate={{ y: [0, -10, 0] }}
         transition={{ repeat: Infinity, duration: 2 }}
-        className="flex justify-center"
+        className="flex item justify-center"
       >
         <Ghost size={100} className="text-secondary" />
       </motion.div>
@@ -19,7 +19,7 @@ export default function NotFoundContent() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
       >
-        <h2 className="mt-6 text-9xl font-extrabold text-button">404</h2>
+        <h2 className="text-9xl font-extrabold text-center text-button">404</h2>
         <h2 className="mt-6 text-3xl font-extrabold">
           ¡Ups! Página no encontrada
         </h2>
@@ -32,17 +32,12 @@ export default function NotFoundContent() {
       >
         La página que estás buscando no existe o ha sido movida.
       </motion.p>
-      <motion.div
-        initial={{ opacity: 0, y: 50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.4, duration: 0.5 }}
-      >
-        <Link href="/">
-          <InglesaiButton variant="secondary">
-            Volver a la Página Principal
-          </InglesaiButton>
-        </Link>
-      </motion.div>
+
+      <Link href="/">
+        <InglesaiButton variant="secondary">
+          Volver a la Página Principal
+        </InglesaiButton>
+      </Link>
     </div>
   );
 }
